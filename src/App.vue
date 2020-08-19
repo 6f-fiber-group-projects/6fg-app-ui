344f65244cd4f7486e1345bd245554ac50de1fe1
mmfiber <masa.03191996@gmail.com>
2020-08-19 17:49:00 +0900

    in progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   v-app
-    router-view 
+    router-view
+    Notification
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import Notification from "@/components/Notification.vue"
 
-@Component
+@Component({components: {Notification}})
 export default class App extends Vue {}
 </script>
---
+++ src/components/Notification.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div hellp
+</template>>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+export default class Notification extends Vue {
+    
+}
+</script>
