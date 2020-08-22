ae39a437f51a9ffac2cfcb28a58d338bb0ff6aef
mmfiber <masa.03191996@gmail.com>
2020-08-22 18:23:37 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:8000/"
 
 Vue.config.productionTip = false
 
